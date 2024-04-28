13c5acd1d4ced3ea6a0d2b3c100fc5e3f4344b42
dfbsh <1293086005@qq.com>
2024-04-28 19:49:03 +0800

    服务器最终版

--- tcp_fuwuqi.py
+++ tcp_fuwuqi.py
@@ -1,19 +1,23 @@
 import socket
 
-tcp_server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-tcp_server.bind(("127.0.0.1",8080))
-tcp_server.listen()
-print("sever starts listen")
-a,b=tcp_server.accept()
-print(f"accept from {b}")
+tcp_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcp_server.bind(("127.0.0.1", 8080))
+tcp_server.listen(5)  # 显式指定连接请求队列的最大长度为 5
+print("服务器开始监听")
+
+conn, addr = tcp_server.accept()
+print(f"接受到来自 {addr} 的连接")
+
 while True:
-    data=a.recv(1024)
-    if data==b'close':
-        print("sever is close")
+    data = conn.recv(1024)
+    if data == b'close':
+        print("服务器已关闭")
         break
+    # if not data:
+    #     print("客服端已经关闭")
+    #     break
     print(data)
-    a.sendall(data)
-a.close()
+    conn.sendall(data)
+
+conn.close()
 tcp_server.close()
-
-
